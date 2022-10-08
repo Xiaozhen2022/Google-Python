5088c1b39eb62253f0586c701e98726674587a14
myf <xiaozhen@gmail.com>
2022-10-08 00:39:27 -0700

    Create course 2

---
+++ Interact_with_OS/test1.py
@@ -0,0 +1,17 @@
+import os
+import datetime
+
+def file_date(filename):
+  # Create the file in the current directory
+  with open(filename,"w") as file:
+    pass
+  timestamp = os.path.getmtime(filename)
+  # Convert the timestamp into a readable format, then into a string
+  date_time = datetime.datetime.fromtimestamp(timestamp).date()
+  
+  # Return just the date portion 
+  # Hint: how many characters are in “yyyy-mm-dd”? 
+  return ("{}".format(date_time))
+
+print(file_date("newfile.txt")) 
+# Should be today's date in the format of yyyy-mm-dd
